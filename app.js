bc815ab18e70707d1c332336e66280ca92313045
Adelaide Craig <adelaide.craig@outlook.com>
2022-03-08 10:52:51 -0600

    restart seed process

--- app.js
+++ app.js
@@ -1,28 +1,46 @@
 let seeds = 24;
 let play1 = seeds;
 let play2 = seeds;
-const gamePlay = document.getElementsByClassName('seed') 
-let currentPit = document.getElementsByClassName('hole')
+const moveSeeds= document.getElementsByClassName('seed') 
+let allPits = document.getElementsByClassName('hole')
+
 let start = null;
+let board = []
+
+//singles out pit
+
+for(let i = 0; i < allPits.length; i++){
+    allPits[i].addEventListener('click', () => {
+        console.log('click')
+    })
+}
+
+// function singlingOutOneSeeds in Pit(){}
+
+for(let i = 0; i < moveSeeds.length; i++){
+    moveSeeds[i].addEventListener('click', () => {
+        console.log('click')
+    })
+}
 
 
-function moveSeeds(){}
-function singlingOutOneSeed(){}
-function storingSeeds(){}
-function determiningWhenRowEmpty(){}
-function countingSeedsWinner (){}
 
-currentPit.addEventListener('click',startGame)   
-        for (let i = start - 1; i >= limit; i--){
-            row[i].addEventListener('click', ()=>{
-                if(play1 === true){
-                    console.log(grid[i])
-                    grid[i].innerHtml =
+
+
+
+// function moveSeeds(){}
+// function storingSeeds(){}
+// function determiningWhenRowEmpty(){}
+// function countingSeedsWinner (){}
+
+// currentPit.addEventListener('click',startGame)   
+//         for (let i = start - 1; i >= limit; i--){
+//             row[i].addEventListener('click', ()=>{
+//                 if(play1 === true){
+//                     console.log(grid[i])
+//                     grid[i].innerHtml =
                     
-                    } 
+               
            
-        } )
-            
-        } 
-
+      
     
