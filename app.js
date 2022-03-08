4604da6ab7253fc53b623d180bbe9b83f7b2f00e
Adelaide Craig <adelaide.craig@outlook.com>
2022-03-07 21:49:05 -0600

    start of functions

---
+++ app.js
@@ -0,0 +1,28 @@
+let seeds = 24;
+let play1 = seeds;
+let play2 = seeds;
+const gamePlay = document.getElementsByClassName('seed') 
+let currentPit = document.getElementsByClassName('hole')
+let start = null;
+
+
+function moveSeeds(){}
+function singlingOutOneSeed(){}
+function storingSeeds(){}
+function determiningWhenRowEmpty(){}
+function countingSeedsWinner (){}
+
+currentPit.addEventListener('click',startGame)   
+        for (let i = start - 1; i >= limit; i--){
+            row[i].addEventListener('click', ()=>{
+                if(play1 === true){
+                    console.log(grid[i])
+                    grid[i].innerHtml =
+                    
+                    } 
+           
+        } )
+            
+        } 
+
+    
