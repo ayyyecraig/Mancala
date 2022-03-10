bd743eb47f31a3e890f8b55be27d540fdb8d2476
Adelaide Craig <adelaide.craig@outlook.com>
2022-03-10 17:45:38 -0600

    cleaning up code

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ for(let i = 0; i < allPits.length; i++){
         ///  targeting pits indivually but also the seeds amount within
         let currentSeeds = parseInt(allPits[i].dataset.seed)
         // hitting and changing hardcode html-- 4-4=0 
-        // allPits[i].innerHTML =  parseInt(allPits[i].dataset.seed) - parseInt(currentSeeds)
         allPits[i].innerHTML = 0
         allPits[i].dataset.seed = 0
         ///interating thru the following pits to add a seed
@@ -22,7 +21,7 @@ for(let i = 0; i < allPits.length; i++){
                 i=0
          } 
         let counter = currentSeeds
-       while(currentSeeds){
+        while(currentSeeds){
          for(let k = 1; k < counter; k++){
          if((i + k) > 11){
                 i = 0
@@ -31,22 +30,18 @@ for(let i = 0; i < allPits.length; i++){
          let nextSeed = parseInt(allPits[i+k].dataset.seed)
             allPits[i+k].dataset.seed = nextSeed + 1
             //changing the hardcoded 4 to +1 to add to the next pits seed count
-        allPits[i+k].innerHTML = allPits[i+k].dataset.seed   
-          
-        }
+        allPits[i+k].innerHTML = allPits[i+k].dataset.seed     
+         }
         currentSeeds--
-    }      
+        }      
            
-   })  
+    })  
 
 }          
             
            
            
 
-//  function determiningWhenRowEmpty(){}           
-    //  if (player1 = allPits.innerHTML.dataset.seed = 0)
-    //         alert ('Game Over!')
 
 
 
@@ -56,18 +51,18 @@ for(let i = 0; i < allPits.length; i++){
 
 
 
-// function storingSeeds(){}
+// function storingSeeds(){} future prospects
 
     
 
-// function countingSeedsWinner (){}
+// function countingSeedsWinner (){}future prospects
 
 
 //button to restart and clear board My second Event listener
     btn1.addEventListener('click', ()=>{
- if (btn1.innerText ==='Press To Clear Board'){
+             if (btn1.innerText ==='Press To Clear Board'){
             btn1.innerText= 'Restarting';
             }if (btn1.innerText ==='Restarting'){
                 window.location.reload();
              } 
- });
+    });
